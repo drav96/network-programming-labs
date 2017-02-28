9b9fa0004adb7e9cd36e8e3f84a74a11537cfa58
Dragos Lupei <dragos.lupei@sens.media>
2017-02-28 22:54:40 +0200

    calculate levenshtein distance

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,16 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first string: ");
+        String s1 = scan.next();
+        System.out.println("Enter second string: ");
+        String s2 = scan.next();
+        System.out.println("The Levenshtein distance between "+s1+" and " + s2+" is "+StringUtils.getLevenshteinDistance(s1, s2));
     }
 }
